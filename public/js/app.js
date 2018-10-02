58b8dc1f37df72008ef84208e4fe1de2508d06fe
Justin Kook <justinkook@gmail.com>
2018-10-02 14:38:53 -0400

    added geocode api

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,57 @@
+const geocode = (e) => {
+    e.preventDefault();
+    let location = document.getElementById('location-input').value
+    axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+                address: location,
+                key: 'AIzaSyAQPXz579UmeXLiAqMxez-ud7xJJgnsxaI'
+            }
+        })
+        .then(function (res) {
+            console.log(res);
+
+            let formattedAddress = res.data.results[0].formatted_address;
+            let formattedAddressOutput =
+                `<ul class="list-group"> 
+                <li class="list-group-item"> ${formattedAddress}</li>
+             </ul>`;
+
+            let addressComponents = res.data.results[0].address_components;
+            let addressComponentsOutput = `<ul class="list-group">`;
+
+            for (let i = 0; i < addressComponents.length; i++) {
+                addressComponentsOutput +=
+                    `<li class="list-group-item"><strong>${addressComponents[i].types[0]}</strong>: ${addressComponents[i].long_name}</li>`;
+            }
+
+            addressComponentsOutput += `</ul>`;
+
+            let lat = res.data.results[0].geometry.location.lat;
+            let lng = res.data.results[0].geometry.location.lng;
+
+            let geometryOutput =
+                `<ul class="list-group"> 
+                <li class="list-group-item"><strong>Latitude</strong> ${lat}</li>
+                <li class="list-group-item"><strong>Longitude</strong> ${lng}</li>
+             </ul>`;
+
+            document.getElementById('formatted-address').innerHTML =
+                formattedAddressOutput;
+
+            document.getElementById('address-components').innerHTML =
+                addressComponentsOutput;
+
+            document.getElementById('geometry').innerHTML =
+                geometryOutput;
+
+        })
+
+        .catch(function (err) {
+            console.log(err);
+        })
+};
+
+
 function initMap() {
 
     var map = new google.maps.Map(
@@ -9,12 +63,6 @@ function initMap() {
             }
 
         });
-
-    google.maps.event.addListener(map, 'click', function (event) {
-        addMarker({
-            coords: event.latLng
-        });
-    });
 
     let markers = [{
             coords: {
@@ -32,10 +80,6 @@ function initMap() {
         }
     ];
 
-    for (let i = 0; i < markers.length; i++) {
-        addMarker(markers[i]);
-    };
-
     function addMarker(props) {
         let marker = new google.maps.Marker({
             position: props.coords,
@@ -50,4 +94,18 @@ function initMap() {
             });
         }
     };
+
+    google.maps.event.addListener(map, 'click', function (event) {
+        addMarker({
+            coords: event.latLng
+        });
+    });
+
+    for (let i = 0; i < markers.length; i++) {
+        addMarker(markers[i]);
+    };
 }
+
+const locationForm = document.getElementById('location-form');
+
+locationForm.addEventListener('submit', geocode);
